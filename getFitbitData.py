f66d6bb5e99ca0d2b50061546a00fd6341d7f8df
David <contact@almliden.com>
2021-01-04 15:51:33 +0100

    Renamed functions and variables

--- getFitbitData.py
+++ getFitbitData.py
@@ -25,71 +25,71 @@ CLIENT_ID=parser.get('Login Parameters', 'CLIENT_ID')
 CLIENT_SECRET=parser.get('Login Parameters', 'CLIENT_SECRET')
 TOKEN_USER_NAME=parser.get('User', 'TOKEN_USER_NAME')
 
-currentDate = datetime.date.today() # + datetime.timedelta(days=1) #datetime.date(2021, 1, 5) ## date of today instead?
-yesterDate = datetime.date.today() - datetime.timedelta(days=1)
-
-def UpdateRefreshToken(TOKEN_DICT):
+def UpdateRefreshToken(token):
   print("Updating Tokens from Callback UpdateRefreshToken")
   db.user_tokens.find_one_and_update(
     {"user": TOKEN_USER_NAME},
     { "$set": { 
-      "ACCESS_TOKEN": TOKEN_DICT['access_token'],
-      "REFRESH_TOKEN": TOKEN_DICT['refresh_token'],
-      "EXPIRES_AT": TOKEN_DICT['expires_at'],
-      "SCOPE": TOKEN_DICT['scope'],
-      "TOKEN_TYPE": TOKEN_DICT['token_type'],
-      "USER_ID": TOKEN_DICT['user_id'],
-      "EXPIRES_IN": TOKEN_DICT['expires_in']
+      "access_token": token['access_token'],
+      "expires_at": token['expires_at'],
+      "expires_in": token['expires_in'],
+      "refresh_token": token['refresh_token'],
+      "scope": token['scope'],
+      "token_type": token['token_type'],
+      "user_id": token['user_id']
       } 
     }
   )
 
 token = db.user_tokens.find_one({"user": TOKEN_USER_NAME})
 try:
-  auth2_client=fitbit.Fitbit(CLIENT_ID,CLIENT_SECRET,oauth2=True,access_token=token['ACCESS_TOKEN'],refresh_token=token['REFRESH_TOKEN'],expires_at=token['EXPIRES_AT'],refresh_cb=UpdateRefreshToken)
+  auth2_client=fitbit.Fitbit(CLIENT_ID,CLIENT_SECRET,oauth2=True,access_token=token['access_token'],refresh_token=token['refresh_token'],expires_at=token['expires_at'],refresh_cb=UpdateRefreshToken)
 except (TypeError) as e:
   print("Catched error: %s" % (e))
 
 def getSleepForDate(currentDate, fitbitClient, database):
   date = currentDate.isoformat()
-  def checkFn():
+  def checkIfAlreadySaved():
     dateadded_sleep = database.sleep.find({"sleep.dateOfSleep": date})
     return list(dateadded_sleep)
-  def goGet():
+  def getFromApi():
     oneDayData_sleep = fitbitClient.get_sleep(currentDate)
     if (len(oneDayData_sleep['sleep']) > 0):
       database.sleep.insert_one(oneDayData_sleep)
       print("Saved sleep data")
     else:
       print("No sleep data available")
-  performRequest(checkFn, goGet, "sleep", date, fitbitClient)
+  performRequest(checkIfAlreadySaved, getFromApi, "sleep", date)
 
 def getHeartForDate(currentDate, fitbitClient, database):
   date = currentDate.isoformat()
-  def checkFn():
+  def checkIfAlreadySaved():
     dateadded_heart = database.heart.find({"activities-heart.dateTime": date})
     return list(dateadded_heart)
-  def goGet():
+  def getFromApi():
     oneDayData_heart = fitbitClient.intraday_time_series('activities/heart', base_date=date, detail_level='1sec')
     if (len(oneDayData_heart['activities-heart']) > 0):
       database.heart.insert_one(oneDayData_heart)
       print("Saved heart data")
     else:
       print("No heart data available")
-  performRequest(checkFn, goGet, "heart", date, fitbitClient)
+  performRequest(checkIfAlreadySaved, getFromApi, "heart", date)
 
-def performRequest(checkFn, requestFn, name, date, fitbitClient):
-  result = checkFn()
+def performRequest(checkExisting, getFromApi, name, date):
+  result = checkExisting()
   if len(result) == 0:
     try:
       print("Getting %s for date %s" % (name, date))
-      requestFn()
+      getFromApi()
     except (Exception) as e:
       print (e)
   else:
     print("Already saved %s for date %s" % (name, date))
 
+currentDate = datetime.date.today() ## + datetime.timedelta(days=1) #datetime.date(2021, 1, 5)
 getSleepForDate(currentDate, auth2_client, db)
+
+yesterDate = datetime.date.today() - datetime.timedelta(days=1)
 getHeartForDate(yesterDate, auth2_client, db)
 
 dbcontext.disconnect()
