f8b732edb45edfe048fe79757b1c42a765184bbc
David <contact@almliden.com>
2021-01-06 17:19:08 +0100

    Helper method for getting time series. Added code that needs to be moved to utility class or similiar.

--- getFitbitData.py
+++ getFitbitData.py
@@ -100,6 +100,20 @@ yesterDate = datetime.date.today() - datetime.timedelta(days=1)
 getHeartForDate(yesterDate, auth2_client, db)
 getStepsForDate(currentDate, auth2_client, db)
 
+def getDates(startDate, endDate):
+  start = datetime.datetime.fromisoformat(startDate)
+  end = datetime.datetime.fromisoformat(endDate)
+  timedelta = end - start
+  days = int(timedelta.total_seconds()/3600/24)+1
+  date_list = [datetime.datetime.isoformat(end - datetime.timedelta(days=x))[0:10] for x in reversed(range(days)) ]
+  return date_list
+
+## Move out to some helper or utility class to sync historic data
+# dates = getDates('2021-01-01', '2021-01-04')
+# for date in dates:
+#   dateToGet = datetime.date.fromisoformat(date)
+#   getStepsForDate(dateToGet, auth2_client, db)
+
 dbcontext.disconnect()
 
 sys.exit()
