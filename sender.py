489b5c0e47fd303fb097e4febe1559f38cfc13b4
David <contact@almliden.com>
2021-01-20 21:59:56 +0100

    Follow Python naming convention in sender

--- sender.py
+++ sender.py
@@ -5,7 +5,7 @@ import configparser
 import random
 
 class EmailSender:
-  emailAdapter = None
+  email_adapter = None
   database = None
   valuephrases_steps = {
     0 : ['Did you wear your fitbit?', 'I believe you can do better than this.', 'Hope you are well!'],
@@ -19,7 +19,7 @@ class EmailSender:
 
   def __init__(self):
     config = EmailAdapterConfigurator()
-    self.emailAdapter = EmailAdapter(config.Config())
+    self.email_adapter = EmailAdapter(config.Config())
     parser=configparser.ConfigParser()
     parser.read('config.ini')
     self.sender=parser.get('Email Daily Health Report', 'EMAIL_DAILY_HEALTH_REPORT_SENDER')
@@ -32,69 +32,69 @@ class EmailSender:
     try:
       self.database = database
       today = date.today().isoformat()
-      queuedAt = datetime.now().isoformat()
-      sentEmails= self.database.emails.find_one({'date': today, 'category': 'daily' })
-      if (sentEmails != None and bool(sentEmails['sent']) == True and not override_check):
+      queued_at = datetime.now().isoformat()
+      sent_emails= self.database.emails.find_one({'date': today, 'category': 'daily' })
+      if (sent_emails != None and bool(sent_emails['sent']) == True and not override_check):
         print('Already sent email: %s' % today)
         return
-      if (sentEmails == None):
-        self.createIntentToSend(queuedAt)
-      lastSyncTimeResult = self.getLastSyncedFromDb(database, device_id)
-      emailParts = {}
-      yesterDate = date.today() - timedelta(days=1)
-      emailParts['steps'] = self.addSteps(yesterDate)
-      emailParts['restingHeartRate'] = self.addHeartRate(yesterDate)
-      emailParts['distance'] = self.addDistance(yesterDate)
-      emailParts['meditateNudge'] = self.addMeditate()
-      emailParts['sleepStatsYesterDay'] = self.addYesterDaySleep(yesterDate)
-      emailParts['batteryLevel'] = self.addBatteryLevel(lastSyncTimeResult['batteryLevel'], lastSyncTimeResult['lastSyncTime'][0:19])
-      sentAt = datetime.now().isoformat()
-      self.send(emailParts)
-      self.confirmEmailSent(queuedAt, sentAt)
+      if (sent_emails == None):
+        self.create_intent_to_send(queued_at)
+      last_sync_time = self.get_last_synced(database, device_id)
+      email_parts = {}
+      yesterdate = date.today() - timedelta(days=1)
+      email_parts['steps'] = self.add_steps(yesterdate)
+      email_parts['restingHeartRate'] = self.add_heartrate(yesterdate)
+      email_parts['distance'] = self.add_distance(yesterdate)
+      email_parts['meditateNudge'] = self.add_meditate()
+      email_parts['sleepStatsYesterDay'] = self.add_yesterday_sleep(yesterdate)
+      email_parts['batteryLevel'] = self.add_battery_level(last_sync_time['batteryLevel'], last_sync_time['lastSyncTime'][0:19])
+      sent_at = datetime.now().isoformat()
+      self.send(email_parts)
+      self.confirm_email_sent(queued_at, sent_at)
     except (Exception):
       print('Something went wrong in sending email.')
   
-  def getLastSyncedFromDb(self, db, deviceId:str):
-    result = db.devices.find_one({'devices.id': deviceId }, { 'devices.batteryLevel': 1, 'devices.lastSyncTime': 1 } )
+  def get_last_synced(self, db, device_id:str):
+    result = db.devices.find_one({'devices.id': device_id }, { 'devices.batteryLevel': 1, 'devices.lastSyncTime': 1 } )
     if (result != None):
       return result['devices'][0]
     return {}
     
-  def createIntentToSend(self, queuedAt: str):
+  def create_intent_to_send(self, queued_at: str):
     try:
-      self.database.emails.insert_one({ 'date': date.today().isoformat(), 'category': 'daily', 'queuedAt': queuedAt, 'sent': False })
+      self.database.emails.insert_one({ 'date': date.today().isoformat(), 'category': 'daily', 'queuedAt': queued_at, 'sent': False })
     except (Exception):
-      print('Exception in createIntentToSend')
+      print('Exception in create_intent_to_send')
   
-  def confirmEmailSent(self, queuedAt:str, sentAt: str):
+  def confirm_email_sent(self, queued_at:str, sentAt: str):
     try:
-      self.database.emails.find_one_and_update({ 'queuedAt': queuedAt}, { '$set': { 'sent': True, 'sentAt': sentAt } })
+      self.database.emails.find_one_and_update({ 'queuedAt': queued_at}, { '$set': { 'sent': True, 'sentAt': sentAt } })
     except (Exception):
-      print('Exception in confirmEmailSent')
+      print('Exception in confirm_email_sent')
 
-  def addBatteryLevel(self, batteryLevel:int, last_synced:str):
+  def add_battery_level(self, battery_level:int, last_synced:str):
     try:
       selected_text = 'Your trackers battery level.'
-      if (batteryLevel < 20):
+      if (battery_level < 20):
         selected_text += ' Kindly try to find some time to charge your device during the day.'
       with open('{folderPath}/batteryLevel.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
-        return fopen.read().format(section_text = selected_text, section_number = batteryLevel, last_synced = last_synced)
+        return fopen.read().format(section_text = selected_text, section_number = battery_level, last_synced = last_synced)
     except (Exception):
-      print('Something went wrong in def addBatteryLevel')
+      print('Something went wrong in def add_battery_level')
       return ''
 
-  def addHeartRate(self, search_date:date):
+  def add_heartrate(self, search_date:date):
     try:
       yesterdate_heart = self.database.heart.find_one({'activities-heart.dateTime' : search_date.isoformat() })
-      heartRate = yesterdate_heart['activities-heart'][0]['value']['restingHeartRate']
-      if (heartRate != None):
+      heartrate = yesterdate_heart['activities-heart'][0]['value']['restingHeartRate']
+      if (heartrate != None):
         with open('{folderPath}/restingHeartRate.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
-          return fopen.read().format(section_text = 'This was your resting heart rate.', section_number = heartRate)
+          return fopen.read().format(section_text = 'This was your resting heart rate.', section_number = heartrate)
     except (Exception):
-      print('Something went wrong in def addHeartRate')
+      print('Something went wrong in def add_heartrate')
       return ''
   
-  def addDistance(self, search_date:date):
+  def add_distance(self, search_date:date):
     try:
       data = self.database.distance.find_one({'activities-distance.dateTime' : search_date.isoformat() })
       distance = data['activities-distance'][0]['value']
@@ -102,40 +102,40 @@ class EmailSender:
         with open('{folderPath}/distance.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
           return fopen.read().format(section_text = 'This is your calculated distance.', section_number = round(float(distance), 3))
     except (Exception):
-      print('Something went wrong in def addDistance')
+      print('Something went wrong in def add_distance')
       return ''
 
-  def addSteps(self, search_date:date):
+  def add_steps(self, search_date:date):
     try:
       data = self.database.steps.find_one({'activities-steps.dateTime' : search_date.isoformat() })
       steps = data['activities-steps'][0]['value']
       if (steps != None):
         keys = self.valuephrases_steps.keys()
-        phraseKey = 0
+        phrase_key = 0
         for k in keys:
           if int(steps) > k:
-            phraseKey = k 
-          elif k > phraseKey:
+            phrase_key = k 
+          elif k > phrase_key:
             break
-        possiblePhrases = self.valuephrases_steps[phraseKey]
-        phraseIndex = random.randrange(0, len(possiblePhrases))
-        selectedPhrase = possiblePhrases[phraseIndex]
+        possible_phrases = self.valuephrases_steps[phrase_key]
+        phrase_index = random.randrange(0, len(possible_phrases))
+        selected_phrase = possible_phrases[phrase_index]
         with open('{folderPath}/totalSteps.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
-          return fopen.read().format(section_text = 'You took this many steps. %s The recommended number of steps per day is 10 000, but we settle for 8000 to keep our goals reasonable.' % selectedPhrase, section_number = steps)
+          return fopen.read().format(section_text = 'You took this many steps. %s The recommended number of steps per day is 10 000, but we settle for 8000 to keep our goals reasonable.' % selected_phrase, section_number = steps)
     except (Exception) as e:
       print(e)
-      print('Something went wrong in def addSteps')
+      print('Something went wrong in def add_steps')
       return ''
 
-  def addMeditate(self):
+  def add_meditate(self):
     try:
       with open('{folderPath}/meditate.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
         return fopen.read().format(section_text='Meditation sets the mood for the day. Do you have time to spare?', button_location='headspace://home', button_text='Take me there!')
     except (Exception):
-      print('Something went wrong in def addMeditate')
+      print('Something went wrong in def add_meditate')
       return ''
   
-  def addYesterDaySleep(self,  search_date:date):
+  def add_yesterday_sleep(self,  search_date:date):
     try:
       data = self.database.sleep.find_one({'sleep.dateOfSleep' : search_date.isoformat() })
       sleep_time_asleep_raw = data['sleep'][0]['startTime']
@@ -148,7 +148,7 @@ class EmailSender:
         with open('{folderPath}/sleep.html'.format(folderPath=self.template_folder), 'r', -1) as fopen:
           return fopen.read().format(sleep_time_asleep = sleep_time_asleep, sleep_duration = sleep_duration)
     except (Exception):
-      print('Something went wrong in def addYesterDaySleep')
+      print('Something went wrong in def add_yesterday_sleep')
       return ''
 
   def send(self, sections: dict):
@@ -168,7 +168,7 @@ class EmailSender:
         button_text='View more stats!',
         button_location=self.buttonHref)
 
-      self.emailAdapter.sendEmail(
+      self.email_adapter.sendEmail(
         sender=self.sender,
         receiver=self.receiver,
         subject='Health Update ' + date.today().isoformat(),
